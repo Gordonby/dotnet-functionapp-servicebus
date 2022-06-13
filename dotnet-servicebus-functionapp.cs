2ed4688e606ec238b53d855f641434adaac1383d
Gordonby <gordon.byers@microsoft.com>
2022-06-13 14:16:01 +0100

    upping version of servicebus
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

--- dotnet-servicebus-functionapp.cs
+++ dotnet-servicebus-functionapp.cs
@@ -5,8 +5,11 @@ using Microsoft.Extensions.Logging;
 
 namespace Company.Function
 {
+    
     public static class ServiceBusTrigger
     {
+        static string QueueName =  Environment.GetEnvironmentVariable("QueueName");
+        
         [FunctionName("ServiceBusTrigger")]
         public static void Run([ServiceBusTrigger("myproj-q",
         Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
