09af7de2bf1893a5e2d6269ee977f3062f787f51
Gordonby <gordon.byers@microsoft.com>
2022-06-13 13:25:19 +0100

    initial commit
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

---
+++ dotnet-servicebus-functionapp.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class ServiceBusTrigger
+    {
+        [FunctionName("ServiceBusTrigger")]
+        public static void Run([ServiceBusTrigger("myproj-q",
+        Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
